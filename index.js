7e4e488394ebd94adbe5f2bb9734cd994d0a3e84
karim rezk <bigkoko882@gmail.com>
2023-03-31 22:46:08 -0400

    Review: Arithmetic Lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 100);
+
+let num3 = 14;
+let num4 = 5;
+let mod = num3 % num4;
+
+let numbers = [1, 4, 3, 20, 6, 8];
+let max = Math.max(...numbers);
